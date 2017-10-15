8db7cbe4252a9ceb2ffcaa1886589a43e7db49fa
Chonlatas Tasanapreechachai <chonlatas.t@gmail.com>
2017-10-15 23:01:06 +0700

    an alien random falling

--- models.py
+++ models.py
@@ -1,22 +1,8 @@
 import random
 import arcade
 
-class ModelSprite(arcade.Sprite):
-    def __init__(self, *args, **kwargs):
-        self.model = kwargs.pop('model', None)
- 
-        super().__init__(*args, **kwargs)
- 
-    def sync_with_model(self):
-        if self.model:
-            self.set_position(self.model.x, self.model.y)
- 
-    def draw(self):
-        self.sync_with_model()
-        super().draw()
-
 class Alien():
-    def __init__(self):
+    def reset_pos(self):
         self.y = 620
         self.x = random.randrange(480)
 
--- space.py
+++ space.py
@@ -1,12 +1,26 @@
 import arcade
 import random
 
-from models import Rocket, World, Alien, ModelSprite
+from models import Rocket, World, Alien
 
 SCREEN_WIDTH = 480
 SCREEN_HEIGHT = 620
 
 MOVEMENT_SPEED = 5
+
+class ModelSprite(arcade.Sprite):
+        def __init__(self, *args, **kwargs):
+            self.model = kwargs.pop('model', None)
+ 
+            super().__init__(*args, **kwargs)
+ 
+        def sync_with_model(self):
+            if self.model:
+                self.set_position(self.model.x, self.model.y)
+ 
+        def draw(self):
+            self.sync_with_model()
+            super().draw()
  
 class SpaceGameWindow(arcade.Window):
     def __init__(self, width, height):
@@ -20,8 +34,10 @@ class SpaceGameWindow(arcade.Window):
 
         #self.rocketsp = arcade.Sprite('images/Rocket2.png')
         self.rocketsp = ModelSprite('images/Rocket2.png', model=self.world.rocket)
+        
         #self.aliensp = arcade.Sprite('images/Alien.png') 
         self.aliensp = ModelSprite('images/Alien.png', model=self.world.alien)
+        
         self.all_sprites_list.append(self.rocketsp)
         self.all_sprites_list.append(self.aliensp)
         #self.alien_list = arcade.SpriteList()
@@ -31,7 +47,6 @@ class SpaceGameWindow(arcade.Window):
     def start_new_game(self):        
         for i in range(50):
             aliensp = self.world.alien
-            #self.aliensp = arcade.Sprite('images/Alien.png')
             #self.aliensp.set_position(self.world.alien.x, self.world.alien.y) 
             aliensp.x = random.randrange(SCREEN_WIDTH)
             aliensp.y = SCREEN_HEIGHT
@@ -42,9 +57,13 @@ class SpaceGameWindow(arcade.Window):
 
     def on_draw(self):
         arcade.start_render()
-        self.all_sprites_list.draw()
+
+        #self.all_sprites_list.draw()
+
+        self.rocketsp.draw()
+        self.aliensp.draw()
         #self.rocketsp.set_position(self.world.rocket.x, self.world.rocket.y)
-        #self.aliensp.set_position(self.world.alien.x, self.world.alien.y) 
+        
         output = "Score: {}".format(self.score)
         arcade.draw_text(output, 10, 600, arcade.color.WHITE, 12)
 
@@ -57,7 +76,6 @@ class SpaceGameWindow(arcade.Window):
     def on_key_release(self, key, modifiers):
         if key == arcade.key.LEFT or key == arcade.key.RIGHT:
             self.world.rocket.delta_x = 0
-
  
 def main():
     window = SpaceGameWindow(SCREEN_WIDTH, SCREEN_HEIGHT)
