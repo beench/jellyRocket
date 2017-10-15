b56a3cc9aadb1b72e3e30f32618b81f61fd5cb3b
Chonlatas Tasanapreechachai <chonlatas.t@gmail.com>
2017-10-15 18:21:29 +0700

    test

--- models.py
+++ models.py
@@ -1,3 +1,19 @@
+import random
+
+class Alien():
+    def __init__(self):
+        self.center_y = 620
+        self.center_x = random.randrange(480)
+
+    '''def reset_pos(self):
+        self.center_y = 620
+        self.center_x = random.randrange(480)'''
+
+    def update(self):
+        self.center_y -= 1
+        '''if self.top < 0:
+            self.reset_pos()'''
+
 class Rocket:
     def __init__(self, world, x, y):
         self.x = x
@@ -18,6 +34,8 @@ class World:
         self.height = height
  
         self.rocket = Rocket(self, width/2, 60)
+        self.alien = Alien()
  
     def update(self, delta):
         self.rocket.move()
+        self.alien.update()
--- space.py
+++ space.py
@@ -1,7 +1,7 @@
 import arcade
 import random
 
-from models import Rocket, World
+from models import Rocket, World, Alien
 
 SCREEN_WIDTH = 480
 SCREEN_HEIGHT = 620
@@ -12,19 +12,37 @@ class SpaceGameWindow(arcade.Window):
     def __init__(self, width, height):
         super().__init__(width, height)
 
-        arcade.set_background_color(arcade.color.BLACK)
- 
-        self.rocketsp = arcade.Sprite('images/Rocket2.png')
+        self.all_sprites_list = None
+        self.alien_list = None
+        self.score = 0
+        self.rocketsp = None
         self.world = World(width, height)
+
+    def start_new_game(self):
+
+        arcade.set_background_color(arcade.color.BLACK)
+
+        self.rocketsp = arcade.Sprite('images/Rocket2.png')
+        self.aliensp = aecade.Sprite('images/Alien.png') 
+        self.all_sprites_list = arcade.SpriteList()
+        self.alien_list = arcade.SpriteList()
+        self.score = 0
+        
+        for i in renge(50):
+            aliensp.world.alien.center_x = random.randrange(SCREEN_WIDTH)
+            aliensp.world.alien.center_y = SCREEN_HEIGHT
+            self.all_sprites_list.append(aliensp)
+            self.alien_list.append(aliensp)
 
     def update(self, delta):
         self.world.update(delta)
 
     def on_draw(self):
         arcade.start_render()
-        self.rocketsp.set_position(self.world.rocket.x, 
-                                 self.world.rocket.y)
+        self.rocketsp.set_position(self.world.rocket.x, self.world.rocket.y)
         self.rocketsp.draw()
+        output = "Score: {}".format(self.score)
+        arcade.draw_text(output, 10, 20, arcade.color.WHITH, 12)
 
     def on_key_press(self, key, modifiers):
         if key == arcade.key.LEFT:
