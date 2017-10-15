297e6e5a9de1bac3fb959a105b4b2954fbafdac2
Chonlatas Tasanapreechachai <chonlatas.t@gmail.com>
2017-10-15 17:27:42 +0700

    delete game.py add space and models

--- models.py
+++ models.py
@@ -1,20 +1,23 @@
 class Rocket:
     def __init__(self, world, x, y):
-        self.world = world
         self.x = x
         self.y = y
- 
-    def update(self, delta):
-        if self.y > self.world.height:
-            self.y = 0
-        self.y += 5
+
+        self.delta_x = 0
+
+    def move(self):
+        # Move left/right
+        self.x += self.delta_x
+
+        if(self.x < 0 or self.x > 480):
+            self.delta_x = 0
 
 class World:
     def __init__(self, width, height):
         self.width = width
         self.height = height
  
-        self.rocket = Rocket(self, 100, 100)
+        self.rocket = Rocket(self, width/2, 60)
  
     def update(self, delta):
-        self.rocket.update(delta)
+        self.rocket.move()
--- space.py
+++ space.py
@@ -1,42 +1,12 @@
 import arcade
-I=False
-J=False
+import random
+
+from models import Rocket, World
+
 SCREEN_WIDTH = 480
 SCREEN_HEIGHT = 620
 
 MOVEMENT_SPEED = 5
-
-class Rocket:
-    def __init__(self, world, x, y):
-        self.x = x
-        self.y = y
-
-        self.change_x = 0
-        self.delta_y = 0
-
-    def move(self):
-        # Move left/right
-        self.x += self.change_x
-
-class World:
-    def __init__(self, width, height):
-        self.width = width
-        self.height = height
- 
-        self.rocket = Rocket(self, 100, 100)
- 
-    def update(self, delta):
-        global I,J,MOVEMENT_SPEED
-        #self.rocket.update(delta)
-        if(I):
-            self.rocket.change_x = -MOVEMENT_SPEED
-        elif(J):
-            self.rocket.change_x = MOVEMENT_SPEED
-        else:
-            self.rocket.change_x = 0
-
-        self.rocket.move()
-
  
 class SpaceGameWindow(arcade.Window):
     def __init__(self, width, height):
@@ -44,34 +14,31 @@ class SpaceGameWindow(arcade.Window):
 
         arcade.set_background_color(arcade.color.BLACK)
  
-        self.rocket = arcade.Sprite('images/Rocket2.png')
+        self.rocketsp = arcade.Sprite('images/Rocket2.png')
         self.world = World(width, height)
 
-       # self.direction = MOVEMENT_SPEED
- 
     def update(self, delta):
         self.world.update(delta)
 
     def on_draw(self):
         arcade.start_render()
-        self.rocket.set_position(self.world.rocket.x, 
+        self.rocketsp.set_position(self.world.rocket.x, 
                                  self.world.rocket.y)
-        self.rocket.draw()
+        self.rocketsp.draw()
 
     def on_key_press(self, key, modifiers):
-        global I,J
         if key == arcade.key.LEFT:
-            I=True
-            J=False
-            #self.rocket.delta_x = -MOVEMENT_SPEED
+            self.world.rocket.delta_x = -MOVEMENT_SPEED
         elif key == arcade.key.RIGHT:
-            I=False
-            J=True
-            self.rocket.delta_x = MOVEMENT_SPEED
+            self.world.rocket.delta_x = MOVEMENT_SPEED
+
+    def on_key_release(self, key, modifiers):
+        if key == arcade.key.LEFT or key == arcade.key.RIGHT:
+            self.world.rocket.delta_x = 0
+
  
- 
-if __name__ == '__main__':
+def main():
     window = SpaceGameWindow(SCREEN_WIDTH, SCREEN_HEIGHT)
     arcade.run()
 
-    main()
+main()
